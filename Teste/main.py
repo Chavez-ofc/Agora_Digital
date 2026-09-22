91c23451a1399450237b66e9b42ac1101c190c32
Chavez-cu <phbbpedrobueno@usp.br>
2026-09-21 23:03:47 -0300

    classe funcionando como esperado

--- Teste/main.py
+++ Teste/main.py
@@ -4,23 +4,26 @@ from datetime import datetime as dt
 import numpy as np
 
 
-url = 'https://xdmqojzrjnicaoxxnmdf.supabase.co'
-key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXFvanpyam5pY2FveHhubWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2ODYzMTcsImV4cCI6MjEwMzI2MjMxN30.RwC39eVuaAVaWmtVEIbdzTcrdHr7Y5-KGTcAvaReUqo'
-
-supabase = create_client(url,key)
-class features(supabase):
-    def __init__(self, critic:str, sugg:str):
+url = "https://xdmqojzrjnicaoxxnmdf.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXFvanpyam5pY2FveHhubWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2ODYzMTcsImV4cCI6MjEwMzI2MjMxN30.RwC39eVuaAVaWmtVEIbdzTcrdHr7Y5-KGTcAvaReUqo"
+sb: Client = create_client(url,key)
+class features:
+    def __init__(self,client: Client, critic:str="", sugg:str=""):
+        self.sb = client
         self.critic = critic
         self.sugg = sugg
 
     def send_critic(self):
-        
+        """Enables to send a string that will be saved in the column 'Critics' from str_bank project at Supabase
+            \nARG: str -> "Text to be sent"
+
+        """
         if not self.critic or not self.critic.strip():
             print("Formato não aceito")
             return
 
         try:
-            response = supabase.table("str_bank",).insert({"Critics": self.critic},returning = "minimal")
+            response = self.sb.table("str_bank",).insert({"Critics": self.critic}, returning = "minimal")
             #a propriedade returning = "minimal" evita que o python tente ler a linha inserida
         
             response.execute()
@@ -36,12 +39,15 @@ class features(supabase):
             return
 
         try:
-             response = supabase.table("str_bank",).insert({"Suggestion": self.sugg},returning = "minimal")
+             response = self.sb.table("str_bank",).insert({"Suggestions": self.sugg}, returning = "minimal")
              response.execute()
              return response
 
         except Exception as e:
             print(e)
+
+app = features(client = sb, critic ="teste1")
+app.send_critic()
 
 """
     def func_buttons(self):
